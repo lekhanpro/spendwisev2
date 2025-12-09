0261d4062ad7e820796ddaec38e56e604b75695d
lekhan <lekhan2007@gmail.com>
2025-12-09 21:50:40 +0530

    Add PWA manifest and service worker for APK generation

---
+++ public/sw.js
@@ -0,0 +1,38 @@
+// Service Worker for SpendWise PWA
+const CACHE_NAME = 'spendwise-v1.0.3';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/logo.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(urlsToCache))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.filter((cacheName) => cacheName !== CACHE_NAME)
+                    .map((cacheName) => caches.delete(cacheName))
+            );
+        })
+    );
+});
